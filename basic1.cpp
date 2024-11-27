7a6e16d8b0e74a1e82aa0bba5b6cb02fb531d98c
tienvuiet <tienxinhzai241@GMAIL.COM>
2024-11-27 20:19:47 +0700

    basic1

---
+++ basic1.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main (){
+	int array[2][2]={{1,2},{3,4}};
+	printf("%d\t",array[0][1]);
+	printf("%d\n",array[0][0]);
+	printf("%d\t",array[1][1]);
+	printf("%d\n",array[1][0]);
+}
+
